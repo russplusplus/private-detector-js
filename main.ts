033ed084b5bff0c69d99f7b4ce809ce93d0772b0
Russell Kerber <russell.kerber@gmail.com>
2024-04-01 12:58:45 -0600

    added comments and typing

--- main.ts
+++ main.ts
@@ -1,66 +1,42 @@
 import * as tf from '@tensorflow/tfjs-node';
-import * as fs from 'fs';
 import { read_image } from './utils/preprocess';
 
-console.log("Hello via Bun!");
+const model_path: string = 'file://model/model.json'
+const file_paths: string[] = [
+    './samples/no/1.jpg',
+    './samples/no/2.jpg',
+    './samples/no/3.jpg',
+    './samples/no/4.jpg',
+    './samples/no/5.jpg',
+    './samples/no/6.jpg',
+    './samples/no/7.jpg',
+    './samples/yes/1.jpg',
+    './samples/yes/2.jpg',
+    './samples/yes/3.jpg'
+]
 
-async function loadModel() {
-    const model = await tf.loadGraphModel('file://model/model.json');
-    console.log("Model loaded. model");
+const probs: Promise<number[]> = RunInference(model_path, file_paths)
+console.log('probs:', probs)
 
-    // const imageBuffer = Buffer.from(base64str, 'base64')
-    // const imageBuffer = Buffer.from('./samples/no/1.jpg')
-    // const imageBuffer = fs.readFileSync('./samples/no/1.jpg')
-    // console.log('imageBuffer:', imageBuffer)
-    // get the tensor
-    // decodeImage can only return 3D or 4D tensor
-    // decomeImage also takes dtype, only int32 is supported currently
-    // const imageTensor = tf.node.decodeImage(imageBuffer)
+export async function RunInference(modelPath: string, filePaths: string[]): Promise<number[]> {
 
-    const filepaths = {
-        no: ['./samples/no/1.jpg',
-            './samples/no/2.jpg',
-            './samples/no/3.jpg',
-            './samples/no/4.jpg',
-            './samples/no/5.jpg',
-            './samples/no/6.jpg',
-            './samples/no/7.jpg'
-        ],
-        yes: ['./samples/yes/1.jpg',
-            './samples/yes/2.jpg',
-            './samples/yes/3.jpg'
-        ]
-    }
+    return new Promise(async (resolve, reject) => {
+        try {
+            const model = await tf.loadGraphModel(modelPath);
 
-
-
-    // imageTensor.reshape([-1, 691200])
-    // const imageTensor2D = 
-
-    // const tensor3D = imageTensor.expandDims(2)
-    // const newJpg = tf.node.encodeJpeg(tensor3D)
-    // input must be tf.float16 2D tensor
-    for (let filepath of filepaths.no) {
-        const imageTensor = await read_image(filepath)
-        const output = await model.predict(imageTensor)
-        const data = await output.data()
-        console.log('NO - output:', data[0]*100, '%')
-    }
-    for (let filepath of filepaths.yes) {
-        const imageTensor = await read_image(filepath)
-        const output = await model.predict(imageTensor)
-        const data = await output.data()
-        console.log('YES - output:', data[0]*100, '%')
-    }
-
-    // const imageTensor = await read_image('./samples/no/1.jpg')
-    // console.log('imageTensor:', imageTensor)
-    // const output = model.predict(imageTensor)
-    // // console.log('output:', output)
-    //     const data = await output.data()
-    //     console.log('NO - output:', data[0]*100, '%')
-
+            let probs: number[] = []
     
+            for (let path of filePaths) {
+                const imageTensor = read_image(path)
+                const output: tf.Tensor3D = model.predict(imageTensor)
+                const data = output.dataSync()
+                probs.push(data[0])
+                console.log('output:', data[0]*100, '%')
+            }
+    
+            resolve(probs)
+        } catch (error) {
+            reject(error)
+        }
+    })
 }
-
-loadModel()
--- utils/preprocess.ts
+++ utils/preprocess.ts
@@ -5,83 +5,53 @@ Module for preprocessing images fed to the model
 import * as tf from '@tensorflow/tfjs-node';
 import * as fs from 'fs';
 
-export async function read_image(filename: string): tf.Tensor {
-    // read the image from the file
-    console.log('in read_image. filename:', filename)
-    const imageBuffer = fs.readFileSync(filename)
-    const imageTensor: tf.Tensor = tf.node.decodeImage(imageBuffer)
-    console.log('imageTensor shape:', imageTensor.shape)
-    // console.log('imageTensor:', await imageTensor.data())
-    const preprocessedImage = preprocess_for_evaluation(imageTensor, 480);
-    console.log('preprocessedImage shape:', preprocessedImage.shape)
-    // console.log('preprocessedImage:', await preprocessedImage.data())
-    const reshapedImage = tf.reshape(preprocessedImage, [-1, 691200]);
-    console.log('reshapedImage shape:', reshapedImage.shape)
+export function read_image(filename: string): tf.Tensor2D {
+
+    const imageBuffer: Buffer = fs.readFileSync(filename)
+
+    // tf.node.decodeImage will always return a 3D tensor when 3 channels are specified, but TypeScript doesn't know this so we need to assert the type
+    const imageTensor: tf.Tensor3D = tf.node.decodeImage(imageBuffer, 3) as tf.Tensor3D
+
+    const preprocessedImage: tf.Tensor3D = preprocess_for_evaluation(imageTensor, 480)
+    
+    const reshapedImage: tf.Tensor2D = tf.reshape(preprocessedImage, [-1, 691200]);
+
     return reshapedImage
 }
 
-export function preprocess_for_evaluation(image: tf.Tensor, image_size: number): tf.Tensor {
+export function preprocess_for_evaluation(image: tf.Tensor3D, image_size: number): tf.Tensor3D {
     
-    console.log('in preprocess_for_evaluation. image shape:', image.shape, '. image_size:', image_size)
-    let tensor = pad_resize_image(image, [image_size, image_size]);
+    const paddedResizedImage: tf.Tensor3D = pad_resize_image(image, [image_size, image_size]);
 
-    // TENSOR IS DIFFERENT THAN IN PYTHON HERE
+    const castImage: tf.Tensor3D = tf.cast(paddedResizedImage, 'float32');
+    const subtractedImage: tf.Tensor3D = castImage.sub(128);
+    const dividedImage: tf.Tensor3D = subtractedImage.div(128);
 
-    // const one_string = image.toString()
-    // fs.writeFile('after_pad_resize.txt', one_string, (err) => {
-    //     if (err) {
-    //         console.log('Error writing after_pad_resize:', err)
-    //     }
-    // })
-    tensor = tf.cast(tensor, 'float32');
-    tensor = tensor.sub(128);
-    tensor = tensor.div(128);
-    return tensor
+    return dividedImage
 }
 
-export function pad_resize_image(image: tf.Tensor, dims: [number, number]): tf.Tensor {
-    console.log('in pad_resize_image. image shape:', image.shape, '. dims:', dims)
-    // // pad the image to make it square
-    // const [height, width] = dims
-    // const [origHeight, origWidth] = image.shape
-
-    // const [height, width] = dims;
-    // const [origHeight, origWidth] = image.shape;
-    // const pad = Math.abs(origHeight - origWidth) / 2;
-    // const padDims = [[0, 0], [pad, pad], [0, 0]];
-    // const paddedImage = tf.pad(image, padDims, 255);
-    // // resize the image to the desired dimensions
-    // const resizedImage = tf.image.resizeBilinear(paddedImage, [height, width]);
-    // return resizedImage;
-
-
-
-    // const [height, width] = dims
-    // const [origHeight, origWidth] = image.shape
-    // const pad = Math.abs(origHeight - origWidth) / 2
-    // const padDims = [[0, 0], [pad, pad], [0, 0]]
-    // const paddedImage = tf.pad(image, padDims, 255)
-
-    // // Because we cannot set preserve_aspect_ratio to true with tensorflowjs, we resize after the image is padded into a square
-    // const resizedImage = tf.image.resizeBilinear(paddedImage, [height, width]);
-
-
-
+export function pad_resize_image(image: tf.Tensor3D, dims: [number, number]): tf.Tensor3D {
+   
+    // aspect ratio = width / height
     const aspectRatio: number = image.shape[1] / image.shape[0];
-    // console.log('aspectRatio:', aspectRatio)
-    const targetHeight: number = dims[0];
-    // console.log('targetHeight:', targetHeight)
-    const targetWidth: number = Math.round(targetHeight * aspectRatio);
-    // console.log('targetWidth:', targetWidth)
 
-    // const one_string = image.toString()
-    // fs.writeFile('before_image_resize.txt', one_string, (err) => {
-    //     if (err) {
-    //         console.log('Error writing after_pad_resize:', err)
-    //     }
-    // })
+    let targetHeight: number
+    let targetWidth: number
 
+    // First, we resize the image so that the longer side is equal to the target size (480), while preserving the aspect ratio.
+    // Because tf.image.resizeBilinear does not support the "preserve_aspect_ratio" argument like in Python, we need to calculate the desired dimensions manually.
+    if (aspectRatio < 1) {
+        targetHeight = dims[0]
+        targetWidth = Math.round(targetHeight * aspectRatio)
+    } else if (aspectRatio > 1) {
+        targetWidth = dims[1]
+        targetHeight = Math.round(targetWidth / aspectRatio)
+    } else {
+        targetHeight = dims[0]
+        targetWidth = dims[1]
+    }
     
+    // halfPixelCenters must be set to true to match the default behavior of tf.image.resize in Python
     let tensor: tf.Tensor3D = tf.image.resizeBilinear(
         image,
         [targetHeight, targetWidth],
@@ -89,65 +59,36 @@ export function pad_resize_image(image: tf.Tensor, dims: [number, number]): tf.T
         true
     )
 
-    // TENSOR IS SAME AS IN PYTHON HERE
-    // THANKS TO halfPixelCenters = true
-
+    // Next, we calculate how much padding is needed to make the image square
     const shape: [number, number, number] = tensor.shape
 
-    const sxd = dims[1] - shape[1]
-    const syd = dims[0] - shape[0]
+    const dWidth: number = dims[1] - shape[1]
+    const dHeight: number = dims[0] - shape[0]
 
-    const sx = tf.cast(
-        sxd / 2,
+    const paddingX: tf.Tensor1D = tf.cast(
+        dWidth / 2,
         'int32'
-    )
-    console.log('sx:', sx.dataSync()[0])
+    ) as tf.Tensor1D 
 
-    const sy = tf.cast(
-        syd / 2,
+    const paddingY: tf.Tensor1D = tf.cast(
+        dHeight / 2,
         'int32'
-    )
-    console.log('sy:', sy.dataSync()[0])
+    ) as tf.Tensor1D
 
-    const a = sy.dataSync()[0]
-    const b = syd - sy.dataSync()[0]
-    const c = sx.dataSync()[0]
-    const d = sxd - sx.dataSync()[0]
+    // Because we are using integer tensors, padding will be asymmetrical if the required total padding is an odd number of cells. 
+    // Therefore, we need to calculate the padding for each side.
+    const paddingT: number = paddingY.dataSync()[0]
+    const paddingB: number = dHeight - paddingY.dataSync()[0]
+    const paddingL: number = paddingX.dataSync()[0]
+    const paddingR: number = dWidth - paddingX.dataSync()[0]
 
-    console.log('a:', a)
-    console.log('b:', b)
-    console.log('c:', c)
-    console.log('d:', d)
-    
-    const paddingsArr = [
-        [a, b],
-        [c, d],
+    const paddingsArr: [[number, number], [number, number], [number, number]] = [
+        [paddingT, paddingB],
+        [paddingL, paddingR],
         [0, 0]
     ]
-    console.log('paddingsArr:', paddingsArr)
-    // const paddings = tf.tensor(paddingsArr, [3,2], 'int32')
-    // const paddingsTensor = paddings.reshape([3,2])
-
-    // const paddings = tf.tensor([
-    //     [sy, syd - sy],
-    //     [sx, sxd - sx],
-    //     [0,0]
-    // ])
-    // console.log('paddingsTensor:', await paddingsTensor.data())
-
-    console.log('tensor shape before pad:', tensor.shape)
-
-    const tensorPadded: tf.Tensor = tf.pad(tensor, paddingsArr, 128)
-
-    console.log('tensor shape after pad:', tensorPadded.shape)
-    
-    // const one_string = image.toString()
-    // fs.writeFile('after_image_resize.txt', one_string, (err) => {
-    //     if (err) {
-    //         console.log('Error writing after_pad_resize:', err)
-    //     }
-    // })
-
+  
+    const tensorPadded: tf.Tensor3D = tf.pad(tensor, paddingsArr, 128)
 
     return tensorPadded
 }
